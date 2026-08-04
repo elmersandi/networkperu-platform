7998725fbabb089b7b24493ddb22df4ca27b0b3f
Elmer Apagueño Sandi <elmersandi16@gmail.com>
2026-08-04 08:34:44 -0500

    cambios de color de textos en el footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -111,7 +111,7 @@ export default function Footer({ config }: Props) {
 
         {/* 🔥 Línea divisoria y Copyright (CON MÁS ESPACIO Y MEJOR DISTRIBUCIÓN) */}
         <div className="border-t border-gray-300 mt-14 pt-8 flex flex-col lg:flex-row justify-between items-center gap-6 text-xs font-medium text-center lg:text-left text-gray-500">
-          <p className="leading-relaxed max-w-2xl">{textoDerechos}</p>
+          <p className=" text-white leading-relaxed max-w-2xl" >{textoDerechos}</p>
           
           <div className="flex flex-wrap justify-center gap-5">
             {/* Ocultamos las redes dinámicamente si no existen */}
